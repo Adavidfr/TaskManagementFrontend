4ecac6fb93bed671d516a6ec919559cd13e82aab
Alex David Frías <adavidfruiz@gmail.com>
2024-09-24 01:09:29 -0500

    feat: add menu

--- src/Page/Sidebar/Sidebar.jsx
+++ src/Page/Sidebar/Sidebar.jsx
@@ -1,10 +1,29 @@
 import { Avatar } from '@mui/material'
-import React from 'react'
-import image1 from './image1.png'; 
+import React, { useState } from 'react'
+import "./Sidebar.css"
+import { Button } from '@mui/material'
+
+const menu = [
+  {name:"Home", value:"Home", role:["ROLE_ADMIN", "ROLE_CUSTOMER"]},
+  {name:"DONE", value:"DONE", role:["ROLE_ADMIN", "ROLE_CUSTOMER"]},
+  {name:"ASSIGNED", value:"ASSIGNED", role:["ROLE_ADMIN"]},
+  {name:"NOT ASSIGNED", value:"PENDING", role:["ROLE_ADMIN"]},
+  {name:"Create New Task", value:"", role:["ROLE_ADMIN"]},
+  {name:"Notification", value:"NOTIFICATION", role:["ROLE_CUSTOMER"]}
+]
+
+const role = "ROLE_ADMIN"
 
 const Sidebar = () => {
+  const [activeMenu, setActiveMenu] = useState ("DONE")
+  const handleMenuChange = (item) => {
+    setActiveMenu(item.name)
+  }
+  const handleLogout = ()=> {
+    console.log("handle logout")
+  }
   return (
-    <div className='min-h-[85vh] flex flex-col justify-center fixed w-[20vw]'>
+    <div className='card min-h-[85vh] flex flex-col justify-center fixed w-[20vw]'>
         <div className='space-y-5 h-full'>
             <div className='flex justify-center'>
                 <Avatar
@@ -13,6 +32,18 @@ const Sidebar = () => {
                     src='https://tr.rbxcdn.com/99efe2b347ba56c51881a79627ca0602/420/420/Hat/Webp' />
 
             </div>
+
+            {
+              menu.filter((item) => item.role.includes(role))
+              .map((item) => <p onClick={()=>handleMenuChange(item)}
+              className={`py-3 px-5 rounded-full text-center cursor-pointer ${activeMenu === item.name?"activeMenuItem":"menuItem"}`}>
+                {item.name}
+              </p>)
+            }
+
+            <Button onClick = {handleLogout} sx={{padding:".7rem", borderRadius:"2rem"}} fullWidth className = 'logoutButton'>
+              logout
+            </Button>
 
         </div>
       
