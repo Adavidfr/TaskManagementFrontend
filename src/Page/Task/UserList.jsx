50cf51cacd9d08434c1f41066b82558cbc4c141f
Alex David Frías <adavidfruiz@gmail.com>
2024-09-26 01:29:21 -0500

    feat: add list items

--- src/Page/Task/UserList.jsx
+++ src/Page/Task/UserList.jsx
@@ -3,6 +3,7 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
+import { Avatar, ListItem, ListItemAvatar, ListItemText } from '@mui/material';
 
 const style = {
   position: 'absolute',
@@ -11,9 +12,9 @@ const style = {
   transform: 'translate(-50%, -50%)',
   width: 400,
   bgcolor: 'background.paper',
-  border: '2px solid #000',
+  outline: "none",
   boxShadow: 24,
-  p: 4,
+  p: 2,
 };
 
 export default function UserList({handleClose, open}) {
@@ -28,12 +29,25 @@ export default function UserList({handleClose, open}) {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
+          {
+            [1,1,1].map((item)=> <div className='flex items-center justify-between w-full'>
+              <div>
+                <ListItem>
+                  <ListItemAvatar>
+                    <Avatar src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUjgjdBYGMGlTaYZT4FiNhQDDf7xlpf-cSQQ&s' />
+                  </ListItemAvatar>
+                  <ListItemText 
+                  secondary="@code_with_david"
+                  primary={"Code With David"}/>
+                </ListItem>
+              </div>
+
+              <div>
+                <Button className='customeButton'>select</Button>
+              </div>
+            </div>)
+          }
+          
         </Box>
       </Modal>
     </div>
