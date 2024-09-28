d9c982ca947072ecf997c1e94c4e92b1eb984f62
Alex David Frías <adavidfruiz@gmail.com>
2024-09-28 02:41:37 -0500

    feat: add submission card

--- src/Page/Task/TaskCard/SubmissionList.jsx
+++ src/Page/Task/TaskCard/SubmissionList.jsx
@@ -3,24 +3,27 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
+import SubmissionCard from './SubmissionCard';
 
 const style = {
   position: 'absolute',
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 400,
+  width: 500,
   bgcolor: 'background.paper',
   border: '2px solid #000',
   boxShadow: 24,
   p: 4,
 };
 
-export default function SubmissionList({handleClose, open}) {
+const submissions = [1, 1, 1]
 
-    return (
+export default function SubmissionList({ handleClose, open }) {
+
+  return (
     <div>
-      
+
       <Modal
         open={open}
         onClose={handleClose}
@@ -28,10 +31,15 @@ export default function SubmissionList({handleClose, open}) {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Submission List
-          </Typography>
-         
+          <div>
+            {submissions.length > 0 ? <SubmissionCard /> : <div className="">
+              <div className='text-center'>
+                No submission Found
+              </div>
+            </div>}
+          </div>
+
+
         </Box>
       </Modal>
     </div>
