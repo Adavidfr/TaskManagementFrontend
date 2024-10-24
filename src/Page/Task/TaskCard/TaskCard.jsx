e5bb9646313db8ee388d3b2961e3d311c640ba08
Alex David Frías <adavidfruiz@gmail.com>
2024-10-24 01:01:56 -0500

    feat: add conecttions

--- src/Page/Task/TaskCard/TaskCard.jsx
+++ src/Page/Task/TaskCard/TaskCard.jsx
@@ -4,13 +4,22 @@ import MoreVertIcon from '@mui/icons-material/MoreVert';
 import UserList from '../UserList';
 import SubmissionList from './SubmissionList';
 import EditTaskForm from './EditTaskForm';
+import { useDispatch } from 'react-redux';
+import { deleteTask } from '../../../ReduxToolkit/TaskSlice';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const role = "ROLE_ADMIN"
-const TaskCard = () => {
+const TaskCard = ({ item }) => {
+    const dispatch = useDispatch();
+
+    const location = useLocation();
+
+    const navigate = useNavigate();
+
     const [anchorEl, setAnchorEl] = React.useState(null);
-   
+
     const openMenu = Boolean(anchorEl);
-   
+
     const handleMenuClick = (event) => {
         setAnchorEl(event.currentTarget);
     };
@@ -21,101 +30,113 @@ const TaskCard = () => {
 
     const [openUserList, setOpenUserList] = useState(false);
 
-    const handleCloseUserList =()=>{
+    const handleCloseUserList = () => {
         setOpenUserList(false)
     }
-   
-    const handleOpenUserList = ()=>{
+
+    const handleOpenUserList = () => {
         setOpenUserList(true);
         handleMenuClose()
     };
 
     const [openSubmissionList, setOpenSubmissionList] = useState(false);
 
-    const handleCloseSubmissionList =()=>{
+    const handleCloseSubmissionList = () => {
         setOpenSubmissionList(false)
     }
 
-    const handleOpenSubmissionList = ()=>{
+    const handleOpenSubmissionList = () => {
         setOpenSubmissionList(true)
         handleMenuClose()
     };
 
     const [openUpdateTaskForm, setOpenUpdateTaskForm] = useState(false);
 
-    const handleCloseUpdateTaskForm =()=>{
+    const handleCloseUpdateTaskForm = () => {
         setOpenUpdateTaskForm(false)
     }
 
-    const handleOpenUpdateTaskModel = ()=>{
-        setOpenUpdateTaskForm(true);
-        handleMenuClose()
+    const handleRemoveTaskIdParams = () => {
+        const updatedParams = new URLSearchParams(location.search)
+        updatedParams.delete("filter")
+        const queryString = updatedParams.toString();
+        const updatedPath = queryString ? `${location.pathname} ? ${queryString}` : location.pathname;
+        navigate(updatedPath);
     }
 
-    const handleDeleteTask = ()=>{
+    const handleOpenUpdateTaskModel = () => {
+        const updatedParams = new URLSearchParams(location.search)
+        setOpenUpdateTaskForm(true);
+        updatedParams.set("taskId", item.id);
+        navigate(`${location.pathname} ? ${updatedParams.toString}`)
+        handleMenuClose();
+    };
+
+    const handleDeleteTask = () => {
+        dispatch(deleteTask(item.id))
         handleMenuClose()
     };
 
-  return (
-    <div>
-        <div className='card lg:flex justify-between'>
-            <div className='lg:flex gap-5 items-center space-y-2 w-[90%] lg:w-[70%]'>
-                <div className=''>
-                    <img className='lg:w-[7rem] lg:h-[rem] object-cover' 
-                    src="https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2022/11/pikachu-pokemon-escarlata-purpura-2888180.jpg?tf=1200x" 
-                    alt="" />
+    return (
+        <div>
+            <div className='card lg:flex justify-between'>
+                <div className='lg:flex gap-5 items-center space-y-2 w-[90%] lg:w-[70%]'>
+                    <div className=''>
+                        <img className='lg:w-[7rem] lg:h-[rem] object-cover'
+                            src={item.image}
+                            alt="" />
+                    </div>
+
+                    <div className='space-y-5'>
+                        <div className='space-y-2'>
+                            <h1 className='font-bold text-lg'>{item.title}</h1>
+                            <p className='text-gray-500 text-sm'>{item.description}</p>
+                        </div>
+
+                        <div className='flex flex-wrap gap-2 items-center'>
+
+                            {item.tags.map((item) => <span className='py-1 px-5 rounded-full techStack'>{item}</span>)}
+
+                        </div>
+                    </div>
+
                 </div>
 
-                <div className='space-y-5'>
-                    <div className='space-y-2'>
-                        <h1 className='font-bold text-lg'>Car Rental Website</h1>
-                        <p className='text-gray-500 text-sm'>use latest frameworks and technology to make this website</p>
-                    </div>
+                <div>
+                    <IconButton id="basic-button"
+                        aria-controls={openMenu ? 'basic-menu' : undefined}
+                        aria-haspopup="true"
+                        aria-expanded={openMenu ? 'true' : undefined}
+                        onClick={handleMenuClick}>
+                        <MoreVertIcon />
+                    </IconButton>
+                    <Menu id="basic-menu" anchorEl={anchorEl}
+                        open={openMenu}
+                        onClose={handleMenuClose}
+                        MenuListProps={{ 'aria-labelledby': 'basic-button', }}>
 
-                    <div className='flex flex-wrap gap-2 items-center'>
 
-                        {[1,1,1,1].map((item)=><span className='py-1 px-5 rounded-full techStack'>Angular</span>)}
+                        {
+                            role === "ROLE_ADMIN" ? <>
+                                <MenuItem onClick={handleOpenUserList}>Assigned User</MenuItem>
+                                <MenuItem onClick={handleOpenSubmissionList}>See Submissions</MenuItem>
+                                <MenuItem onClick={handleOpenUpdateTaskModel}>Edit</MenuItem>
+                                <MenuItem onClick={handleDeleteTask}>Delete</MenuItem>
+                            </> : (<>
+                            </>
+                            )}
 
-                    </div>
+                    </Menu>
                 </div>
 
             </div>
 
-            <div>
-                <IconButton id="basic-button" 
-                            aria-controls={openMenu ? 'basic-menu' : undefined}
-                            aria-haspopup="true"
-                            aria-expanded={openMenu ? 'true' : undefined}
-                            onClick={handleMenuClick}>
-                    <MoreVertIcon/>
-                </IconButton>
-                <Menu id="basic-menu" anchorEl={anchorEl}
-                open={openMenu}
-                onClose={handleMenuClose}
-                MenuListProps={{'aria-labelledby' : 'basic-button',}}>
-                    
-
-                    {
-                        role === "ROLE_ADMIN"?<>
-                        <MenuItem onClick={handleOpenUserList}>Assigned User</MenuItem>
-                        <MenuItem onClick={handleOpenSubmissionList}>See Submissions</MenuItem>
-                        <MenuItem onClick={handleOpenUpdateTaskModel}>Edit</MenuItem>
-                        <MenuItem onClick={handleDeleteTask}>Delete</MenuItem>
-                        </>:(<>
-                        </>
-                    )}
-
-                </Menu>
-            </div>
+            <UserList open={openUserList} handleClose={handleCloseUserList} />
+            <SubmissionList open={openSubmissionList} handleClose={handleCloseSubmissionList} />
+            <EditTaskForm item={item} open={openUpdateTaskForm} handleClose={handleCloseUpdateTaskForm} />
 
         </div>
-
-        <UserList open={openUserList} handleClose={handleCloseUserList}/>
-        <SubmissionList open={openSubmissionList} handleClose={handleCloseSubmissionList}/>
-        <EditTaskForm open={openUpdateTaskForm} handleClose={handleCloseUpdateTaskForm}/>
-
-    </div>
-  )
-}
+    );
+};
 
 export default TaskCard
