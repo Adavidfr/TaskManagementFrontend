4facde1a956a0c244ea9c9fbabddb2470fdb85e2
Alex David Frías <adavidfruiz@gmail.com>
2024-10-24 00:59:26 -0500

    feat: add auth user

--- src/Page/Navbar/Navbar.jsx
+++ src/Page/Navbar/Navbar.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { Avatar } from "@mui/material";
 import "./Navbar.css"
+import { useSelector } from "react-redux";
+import store from "../../ReduxToolkit/Store";
 
 const Navbar = () => {
+    const {task, auth} = useSelector(store => store)
     return (
         <div className='container z-10 sticky left-0 right-0 top-0 py-3 px-5 lg:px-10, flex justify-between items-center'>
 
             <p className='font-bold text-lg'>David Task Manager</p>
 
             <div className='flex items-center gap-5'>
-                <p>codewithdavid</p>
+                <p>{auth.user?.fullName}</p>
                 <Avatar sx={{backgroundColor:"#c24dd0"}} className='bg-[#c24dd0]'>D</Avatar>
             </div>
         </div>
