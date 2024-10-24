f871196f6d704365d5327d6e93a354e5e2ab2c8b
Alex David Frías <adavidfruiz@gmail.com>
2024-10-24 00:58:04 -0500

    feat: add functions app

--- src/App.js
+++ src/App.js
@@ -3,18 +3,34 @@ import { darkTheme } from './theme/darktheme';
 import Navbar from './Page/Navbar/Navbar';
 import Home from './Page/Home/Home';
 import Auth from './Page/Auth/Auth';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchTasks } from './ReduxToolkit/TaskSlice';
+import { getUserProfile } from './ReduxToolkit/AuthSlice';
 
 function App() {
-  const user = useState(true);
+  const user = true;
+  const dispatch = useDispatch();
+  const { task, auth } = useSelector(store => store)
+
+  useEffect(() => {
+    dispatch(fetchTasks({}));
+    dispatch(getUserProfile(auth.jwt || localStorage.getItem("jwt")));
+  }, [auth.jwt]);
   return (
     <ThemeProvider theme={darkTheme}>
-      {user ? <div>
+      {auth.user ? <div>
         <Navbar />
         <Home />
       </div> : <Auth />}
 
     </ThemeProvider>
+
+    // <ThemeProvider theme={darkTheme}>
+    //   {/* Aquí omites la condición de autenticación */}
+    //   <Navbar />
+    //   <Home />
+    // </ThemeProvider>
   );
 }
 
